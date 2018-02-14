a2b905e4bd542464c35a2cd9deec910c32b658b0
Gaotianyu <513884454@qq.com>
2018-02-14 17:02:36 +0800

    详情显示和切换的图标变换

--- app/src/main/java/com/example/gaotianyu/app/Activity/Activity/MainActivity.java
+++ app/src/main/java/com/example/gaotianyu/app/Activity/Activity/MainActivity.java
@@ -22,6 +22,16 @@ import com.example.gaotianyu.app.Activity.Tools.ButtonSlop;
 import com.example.gaotianyu.app.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private SaishiFragment saishiFragment;
+    private LifeFragment lifeFragment;
+    private StudyFragment studyFragment;
+    private int nowFragment = 0;
+    private final int SAISHIFRAGMENT = 0;
+    private final int LIFEFRAGMENT = 1;
+    private final int STUDYFRAGMNET = 2;
+    private ImageView button_saishi;
+    private ImageView button_life;
+    private ImageView button_study;
 
 private DrawerLayout drawerLayout;
     @Override
@@ -37,6 +47,7 @@ private DrawerLayout drawerLayout;
             actionBar.setHomeAsUpIndicator(R.mipmap.menu);
         }
 
+
 /*
         ImageView button_message = (ImageView) findViewById(R.id.button_message);
         button_message.setOnClickListener( new View.OnClickListener(){
@@ -47,17 +58,26 @@ private DrawerLayout drawerLayout;
             }
         });
         */
-        ImageView button_saishi = (ImageView) findViewById(R.id.button_saishi);
+        button_saishi = (ImageView) findViewById(R.id.button_saishi);
         button_saishi.setOnClickListener(this);
-        ImageView button_life = (ImageView) findViewById(R.id.button_life);
+        button_life = (ImageView) findViewById(R.id.button_life);
         button_life.setOnClickListener(this);
-        ImageView button_study = (ImageView) findViewById(R.id.button_study);
+        button_study = (ImageView) findViewById(R.id.button_study);
         button_study.setOnClickListener(this);
-        button_saishi.callOnClick();
         /*ImageView button_geren = (ImageView) findViewById(R.id.button_geren);
         button_geren.setOnClickListener(this);
         button_geren.callOnClick();
         */
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        if (saishiFragment == null){
+            saishiFragment = new SaishiFragment();
+            transaction.add(R.id.fragment_layout, saishiFragment);
+        }else {
+            transaction.show(saishiFragment);
+        }
+        transaction.commit();
+        button_saishi.setImageResource(R.mipmap.click1);
     }
 
     private void replaceFragment(Fragment fragment) {
@@ -71,29 +91,43 @@ private DrawerLayout drawerLayout;
         //TextView title = (TextView)findViewById(R.id.text_title);
         switch (v.getId()){
             case R.id.button_saishi:
+                if (nowFragment == SAISHIFRAGMENT){
+                    return;
+                }
                 //title.setText("赛事");
                 if (ButtonSlop.check(R.id.button_saishi)) {
                     //Toast.makeText(PostActivity.this,"请稍后尝试",Toast.LENGTH_SHORT).show();
                     return;
                 }
-
-                replaceFragment(new SaishiFragment());
+                showFragment(SAISHIFRAGMENT);
+                button_saishi.setImageResource(R.mipmap.click1);
+                //replaceFragment(new SaishiFragment());
                 break;
             case R.id.button_life:
+                if (nowFragment == LIFEFRAGMENT){
+                    return;
+                }
                 if (ButtonSlop.check(R.id.button_life)) {
                     //Toast.makeText(PostActivity.this,"请稍后尝试",Toast.LENGTH_SHORT).show();
                     return;
                 }
+                showFragment(LIFEFRAGMENT);
+                button_life.setImageResource(R.mipmap.click2);
                 //title.setText("生活助手");
-                replaceFragment(new LifeFragment());
+                //replaceFragment(new LifeFragment());
                 break;
             case R.id.button_study:
+                if (nowFragment == STUDYFRAGMNET){
+                    return;
+                }
                 if (ButtonSlop.check(R.id.button_study)) {
                     //Toast.makeText(PostActivity.this,"请稍后尝试",Toast.LENGTH_SHORT).show();
                     return;
                 }
+                showFragment(STUDYFRAGMNET);
+                button_study.setImageResource(R.mipmap.click3);
                 //title.setText("学术交流");
-                replaceFragment(new StudyFragment());
+                //replaceFragment(new StudyFragment());
                 break;
                 /*
             case R.id.button_geren:
@@ -127,6 +161,58 @@ private DrawerLayout drawerLayout;
 
         }
         return true;
+    }
+    private void showFragment(int x){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        hideFragment(transaction);
+        orignalImage();
+        switch (x){
+            case SAISHIFRAGMENT:
+                if (saishiFragment == null){
+                    saishiFragment = new SaishiFragment();
+                    transaction.add(R.id.fragment_layout, saishiFragment);
+                }else {
+                    transaction.show(saishiFragment);
+                }
+                break;
+            case LIFEFRAGMENT:
+                if (lifeFragment == null){
+                    lifeFragment = new LifeFragment();
+                    transaction.add(R.id.fragment_layout, lifeFragment);
+                }else {
+                    transaction.show(lifeFragment);
+                }
+                break;
+            case STUDYFRAGMNET:
+                if (studyFragment == null){
+                    studyFragment = new StudyFragment();
+                    transaction.add(R.id.fragment_layout, studyFragment);
+                }else {
+                    transaction.show(studyFragment);
+                }
+                break;
+        }
+        transaction.commit();
+        nowFragment = x;
+
+    }
+    private void hideFragment(FragmentTransaction transaction){
+        if(saishiFragment!=null){
+            transaction.hide(saishiFragment);
+        }
+        if (lifeFragment!=null){
+            transaction.hide(lifeFragment);
+        }
+        if (studyFragment!=null){
+            transaction.hide(studyFragment);
+        }
+    }
+    private void orignalImage(){
+        button_saishi.setImageResource(R.mipmap.saishi);
+        button_life.setImageResource(R.mipmap.life);
+        button_study.setImageResource(R.mipmap.study);
+
     }
 
 }
--- app/src/main/java/com/example/gaotianyu/app/Activity/Activity/ShowActivity.java
+++ app/src/main/java/com/example/gaotianyu/app/Activity/Activity/ShowActivity.java
@@ -6,11 +6,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
+import com.example.gaotianyu.app.Activity.PostList.PostList;
 import com.example.gaotianyu.app.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ShowActivity extends AppCompatActivity {
+    private CircleImageView touxiang;
+    private TextView nickName;
+    private TextView time;
+    private TextView title;
+    private TextView label1;
+    private TextView label2;
+    private TextView main;
+    private String kind;
+    private PostList post;
+
+
+
+
 
 
 
@@ -29,10 +46,29 @@ public class ShowActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        String kind = intent.getStringExtra("kind");
+        kind = intent.getStringExtra("kind");
+        initView();
+        post = (PostList)intent.getSerializableExtra("post_data");
+        initData();
 
+    }
+    /*
+    加载布局和控件
+     */
+    private void initView(){
+        if (kind.equals("3")){
+            setContentView(R.layout.activity_show3);
+            label2 = (TextView) findViewById(R.id.label2);
+        }else {
+            setContentView(R.layout.activity_show12);
+        }
+        touxiang = (CircleImageView)findViewById(R.id.touxiang);
+        nickName = (TextView) findViewById(R.id.nickname);
+        time = (TextView) findViewById(R.id.time);
+        label1 = (TextView) findViewById(R.id.label1);
+        title = (TextView) findViewById(R.id.title);
+        main = (TextView) findViewById(R.id.main);
 
-        setContentView(R.layout.activity_show12);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
@@ -40,8 +76,26 @@ public class ShowActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.mipmap.back);
         }
+    }
+    private void initData(){
+        time.setText(post.getUpdatedAt());
+        String label= post.getLabel();
+        if (kind.equals("3")){
+            for (int i=0;i<label.length();i++){
+                if (label.charAt(i)=='#'){
+                    label1.setText(label.substring(0,i));
+                    label2.setText(label.substring(i+1));
+                }
+            }
+        }else {
+            label1.setText(label);
+        }
+        title.setText(post.getTitle());
+        main.setText(post.getContent());
+
 
     }
+
 
 
 }
